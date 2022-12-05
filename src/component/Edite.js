562c6d65b3735541c67248856f639820eb0c1e6d
camila leal <khammylv@gmail.com>
2022-12-05 12:03:39 -0500

    OH test

--- src/component/Edite.js
+++ src/component/Edite.js
@@ -3,6 +3,11 @@ import {
   updateTask,
 } from '../lib/index.js';
 
+const updateTaskHandler = (id, tt) => {
+  updateTask(id, {
+    post: tt.value,
+  });
+};
 export const EditePost = () => {
   const poste = document.querySelector('.cajapost');
   const editbtn = poste.querySelectorAll('.edit_img');
@@ -25,11 +30,7 @@ export const EditePost = () => {
 
           const btnsend = cartaInd.querySelector('.btnSaveImage');
           // eslint-disable-next-line no-loop-func
-          btnsend.addEventListener('click', () => {
-            updateTask(id, {
-              post: tt.value,
-            });
-          });
+          btnsend.addEventListener('click', () => updateTaskHandler(id, tt));
         }
       }
     });
